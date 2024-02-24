60eeb78c02396247a1fe56547c20815402a5731b
Jagadeesh <jagadeesh@kali.Jagadeesh>
2024-02-24 18:52:10 +0530

    adding mango bd

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 const db = mongoose.connection;
 
